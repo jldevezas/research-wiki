36c5983d54a0256ba6c0def8f58e6d655c1d657f
José Devezas <joseluisdevezas@gmail.com>
2020-11-17 13:04:58 +0000

    Wiki: Extension installation now works.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM debian:buster-slim
 
 RUN apt-get update && apt-get -y dist-upgrade
-RUN apt-get -y install wget rsync
+RUN apt-get -y install wget rsync unzip
 
 COPY scripts scripts
 COPY skeleton skeleton
--- scripts/install_dokuwiki.sh
+++ scripts/install_dokuwiki.sh
@@ -2,7 +2,29 @@
 
 INSTALL_DIR=/var/www/html
 
-if [ ! -z "$(ls -A ${INSTALL_DIR})" ]
+read -d " " PLUGINS << EOF
+https://github.com/selfthinker/dokuwiki_plugin_wrap/archive/stable.zip
+EOF
+# https://github.com/leibler/dokuwiki-plugin-todo/archive/stable.zip
+# https://github.com/kn0/dokuwiki_timestamp/zipball/master
+# https://github.com/dwp-forge/tablewidth/archive/v.2016-10-13.zip
+# https://github.com/FyiurAmron/sortablejs/zipball/master
+# https://github.com/michitux/dokuwiki-plugin-saveandedit/zipball/master
+# https://github.com/dwp-forge/refnotes/archive/v.2020-08-10.zip
+# https://github.com/ssahara/dw-plugin-NumberedHeadings/archive/master.zip
+# https://github.com/gturri/nspages/zipball/master
+# https://github.com/cosmocode/nslist/zipball/master
+# https://github.com/michitux/dokuwiki-plugin-move/zipball/master
+# https://github.com/liffiton/dokuwiki-plugin-mathjax/archive/master.zip
+# https://github.com/flammy/imagebox/tarball/master
+# https://github.com/dokufreaks/plugin-folded/archive/master.zip
+# https://github.com/cosmocode/edittable/archive/master.zip
+# https://github.com/splitbrain/dokuwiki-plugin-dw2pdf/tarball/master
+# https://github.com/dokufreaks/plugin-blockquote/archive/master.zip
+# https://github.com/tatewake/dokuwiki-plugin-backup/archive/master.zip
+# https://github.com/mprins/dokuwiki-plugin-backlinks/archive/master.zip
+
+if [ ! -z "$(ls ${INSTALL_DIR})" ]
 then
     echo "${INSTALL_DIR}: directory is not empty (delete files for a clean install;" \
         "otherwise the current files will be served)"
@@ -17,3 +39,22 @@ chown -R www-data:www-data dokuwiki*
 chmod -R 0700 dokuwiki*/{data,conf,bin,inc,vendor}
 mv dokuwiki*/* /var/www/html
 rsync --ignore-existing -a skeleton/* /var/www/html/data/pages
+
+echo "Installing DokuWiki extensions required for the research wiki"
+
+for plugin in $PLUGINS
+do
+    wget $plugin -O plugin.zip
+    unzip plugin.zip -d /var/www/html/lib/plugins
+done
+
+for plugin_info in $(find /var/www/html/lib/plugins -name plugin.info.txt)
+do
+    plugin_dir=$(dirname $plugin_info)
+    new_plugin_dir=$(dirname $plugin_dir)/$(grep "^base" $plugin_info | awk '{ print $2 }')
+
+    if [[ $plugin_dir != $new_plugin_dir ]]
+    then
+        mv -v $plugin_dir $new_plugin_dir
+    fi
+done
